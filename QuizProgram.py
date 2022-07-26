b871341df2e17f11aa68cca5d2e05ad25f2195b8
avnibar <82066521+avnibar@users.noreply.github.com>
2022-07-26 14:52:29 +1200

    Add files via upload

--- QuizProgram.py
+++ QuizProgram.py
@@ -1,14 +1,23 @@
+from email import message
 import random
 from socket import getprotobyname
 from tkinter import *
 from tkinter.font import Font
-questionList = [["Astrology is a pseudoscience.","True","False"],["Why does the moon have a bigger effect on the tides than the sun?","It is closer to Earth","It is bigger","It is smaller","It is further away from Earth"],["What is a solar eclipse?","When the sun is behind the moon","When Earth is in between the moon and sun","When the moon is behind the sun","When the sun is below Earth"],["What is a lunar eclipse?","When Earth is in between the moon and sun","When the sun is behind the moon","When the moon is behind the sun","When the sun is below Earth"],["How long does it take for the moon to complete 1 revolution around the Earth?","29.5 Days","30 Days","24 Hours","28 Days"],["How many km is 1 Astronomical Unit (A.U.)?","150,000,000 km","150,000 km","100,000,000 km","100,000 km"],["When the moon phase is waning it means it is getting bigger.","False","True"],["The New Moon is a moon phase where the moon is completely dark.","True","False"],["The moon produces its own light.","False","True"],["How many high tides are there on average per 24 hour day?","2","1","3","4"],["The area of partial shadow of the Earth is called?","Penumbra","Umbra","Terrumbra","Sombra"],["The area of complete shadow of the Earth is called?","Umbra","Penumbra","Terrumbra","Sombra"],["What causes the Earth's seasons?","The Earth’s tilt","The distance from the sun to the Earth","The Earth’s speed around the sun","The Earth’s distance from the moon"],["Astronomy is a pseudoscience.","False","True"],["How many phases of the moon are there?","8","6","10","12"],["The term ‘Gibbous’ is used to describe the moon when it is…","Almost full","Half full","Full","Almost empty"],["The sun’s core fuses Hydrogen into _____ when releasing energy.","Helium","Nitrogen","Oxygen","Neon"],["How many astronomical units away is the sun from the Earth?","1","2","5","8"],["How many planets do we have in our solar system?","8","6","7","9"],["Pluto is not a planet.","True","False"]]
-messageList = ["UNLUCKY!","GOOD EFFORT!","NICE JOB!","INCREDIBLE!"]
-gradeList = ["NOT ACHIEVED","ACHIEVED","MERIT","EXCELLENCE"]
-scoreRequire = [0, 4, 7, 10, 12]
-global score
-score = 0
-questionList = random.sample(questionList, len(questionList))
+
+def initialising():
+    global questionList
+    questionList = [["Astrology is a pseudoscience.","True","False"],["Why does the moon have a bigger effect on the tides than the sun?","It is closer to Earth","It is bigger","It is smaller","It is further away from Earth"],["What is a solar eclipse?","When the sun is behind the moon","When Earth is in between the moon and sun","When the moon is behind the sun","When the sun is below Earth"],["What is a lunar eclipse?","When Earth is in between the moon and sun","When the sun is behind the moon","When the moon is behind the sun","When the sun is below Earth"],["How long does it take for the moon to complete 1 revolution around the Earth?","29.5 Days","30 Days","24 Hours","28 Days"],["How many km is 1 Astronomical Unit (A.U.)?","150,000,000 km","150,000 km","100,000,000 km","100,000 km"],["When the moon phase is waning it means it is getting bigger.","False","True"],["The New Moon is a moon phase where the moon is completely dark.","True","False"],["The moon produces its own light.","False","True"],["How many high tides are there on average per 24 hour day?","2","1","3","4"],["The area of partial shadow of the Earth is called?","Penumbra","Umbra","Terrumbra","Sombra"],["The area of complete shadow of the Earth is called?","Umbra","Penumbra","Terrumbra","Sombra"],["What causes the Earth's seasons?","The Earth’s tilt","The distance from the sun to the Earth","The Earth’s speed around the sun","The Earth’s distance from the moon"],["Astronomy is a pseudoscience.","False","True"],["How many phases of the moon are there?","8","6","10","12"],["The term ‘Gibbous’ is used to describe the moon when it is…","Almost full","Half full","Full","Almost empty"],["The sun’s core fuses Hydrogen into _____ when releasing energy.","Helium","Nitrogen","Oxygen","Neon"],["How many astronomical units away is the sun from the Earth?","1","2","5","8"],["How many planets do we have in our solar system?","8","6","7","9"],["Pluto is not a planet.","True","False"]]
+    global messageList
+    messageList = ["UNLUCKY!","GOOD EFFORT!","NICE JOB!","INCREDIBLE!"]
+    global gradeList
+    gradeList = ["NOT ACHIEVED","ACHIEVED","MERIT","EXCELLENCE"]
+    global scoreRequire
+    scoreRequire = [0, 4, 7, 10, 12]
+    global score
+    score = 0
+    questionList = random.sample(questionList, len(questionList))
+
+    titleScreen()
 
 def calcGrade():
     
@@ -31,11 +40,22 @@ def calcGrade():
     groot.geometry("500x555")
     groot.title("Final Grade")
     pageFont = Font(family = "Gill Sans MT",weight="bold",size=18)
+    playAgainFont = Font(family = "Gill Sans MT",weight="bold",size=16)
     
     title = Label(groot, text="YEAR 10 SCIENCE QUIZ", font=pageFont, bg="#fd5050", fg="#FFFFFF",padx=5,pady=50)
     title.place(x=250,y=80, anchor="center")
     messageBox = Label(groot, text="YOUR SCORE: "+str(score)+"/12\nYOUR GRADE: "+str(finalGrade)+"\n\n"+str(message), font=pageFont, bg="#fd5050", fg="#FFFFFF",padx=5,pady=50)
-    messageBox.place(x=250,y=300, anchor="center")
+    messageBox.place(x=250,y=300, anchor="center")                 
+
+    exitButton = Button(groot, text="EXIT", font=pageFont, bg="#fd5050", fg="#FFFFFF", command=exit, borderwidth=1, padx=40, pady=10)
+    exitButton.place(x=318, y=460)
+
+    playAgain = Button(groot, text="PLAY AGAIN", font=playAgainFont, bg="#fd5050", fg="#FFFFFF", borderwidth=1, padx=4, pady=13, command=restart)
+    playAgain.place(x=20, y=460)
+
+def restart():
+    groot.destroy()
+    initialising()
 
 def answerPhase():
     root.destroy()
@@ -62,7 +82,7 @@ def answerPhase():
 
         qNumber = Label(qroot, text="QUESTION "+str(questionLCV+1-skipCounter)+".", font=pageFont, fg="white", bg="#fd5050",padx=20,pady=20)
         qNumber.place(x=15,y=15)
-
+        
         questionText = Label(qroot, text=questionList[questionLCV][0], font=pageFont, fg="white", bg="#fd5050", padx=20, pady=20)
         questionText.place(x=500, y=175, anchor="center")
 
@@ -151,6 +171,7 @@ def answerClick(ansNum):
 def next():
     qroot.destroy()
 
+
 def titleScreen():
     global root
     root = Tk()
@@ -163,14 +184,16 @@ def titleScreen():
     title = Label(root, text="YEAR 10 SCIENCE QUIZ", font=pageFont, bg="#fd5050", fg="#FFFFFF",padx=5,pady=50)
     title.place(x=173,y=50)
     
-    #Image here
+    logoFile = PhotoImage(file="icon.png")
+    logoInput = Label(root, image=logoFile, bg="#ffd3a6")
+    logoInput.place(x=330,y=300, anchor="center")
 
     playButton = Button(root, text="PLAY", font=pageFont, bg="#fd5050", fg="#FFFFFF", command=answerPhase, borderwidth=1, padx=40, pady=10)
-    playButton.place(x=100, y=400)
+    playButton.place(x=95, y=410)
     exitButton = Button(root, text="EXIT", font=pageFont, bg="#fd5050", fg="#FFFFFF", command=exit, borderwidth=1, padx=40, pady=10)
-    exitButton.place(x=400, y=400)
+    exitButton.place(x=395, y=410)
+
 
     root.mainloop()
 
-
-titleScreen()
+initialising()
